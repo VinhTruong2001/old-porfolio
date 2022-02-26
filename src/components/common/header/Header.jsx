a3773ecda07950d6c2134f757e44c6e73f92b5eb
VinhTruong2001 <ducvinh.truong2001@gmail.com>
2022-02-26 21:39:46 +0700

    complete 3D nav template

---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,21 @@
+import './header.scss'
+
+function Header({ active, setActive }) {
+    return (
+		<div className="header">
+			<div className="header__content">
+				<div className="logo">3DNav</div>
+				<div
+					className={ `menu-toggle ${active ? 'active' : ''}` }
+					onClick={ () => setActive(!active) }
+				>
+					<i className='bx bx-menu'></i>
+					<i className='bx bx-x'></i>
+
+				</div>
+			</div>
+		</div>
+    )
+}
+
+export default Header
